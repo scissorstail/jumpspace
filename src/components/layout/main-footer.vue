dc55f0bfdfe8d4fb378d8811f5923ca8adcf4763
nameEO <nameEO>
2020-08-01 22:09:38 +0900

    레이아웃 세팅

---
+++ src/components/layout/main-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'MainFooter'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,17 +1,56 @@
 <template>
-    <Editor></Editor>
+  <div id="layout">
+    <MainHeader id="main-header"></MainHeader>
+    <MainNavigator id="main-navigator"></MainNavigator>
+    <div id="main-editor">
+      <Editor></Editor>
+    </div>
+    <MainFooter id="main-footer"></MainFooter>
+  </div>
 </template>
 
 <script>
-import Editor from '../components/Editor/index'
+import MainHeader from '../components/layout/main-header'
+import MainNavigator from '../components/layout/main-navigator'
+import MainFooter from '../components/layout/main-footer'
+import Editor from '../components/editor/index'
 
 export default {
   name: 'Layout',
   components: {
+    MainHeader,
+    MainNavigator,
+    MainFooter,
     Editor
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+#layout {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+#main-header {
+  display: flex;
+  flex: 1;
+  background-color: red
+}
+#main-navigator {
+  display: flex;
+  flex: 1;
+  background-color: blue
+}
+#main-editor {
+  display: flex;
+  flex: 1;
+  background-color: white
+}
+#main-footer {
+  display: flex;
+  flex: 1;
+  background-color: green
+}
 </style>
