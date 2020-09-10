fb5fb925a08a954111cdfa6d1738d4598e060e27
nameEO <nameEO>
2020-09-11 03:11:07 +0900

    프로젝트 import 시 로컬스토리지에 저장처리

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -118,6 +118,7 @@ export default {
     async loadProject (projectSaveData) {
       if (projectSaveData) {
         this.projectData = JSON.parse(projectSaveData)
+        window.localStorage.projectSaveData = projectSaveData
       } else {
         this.projectData = []
       }
