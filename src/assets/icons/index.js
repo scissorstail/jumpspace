afb897af8c67f4a65e1919b55a782b0977feaa3c
ksm <pocket.ksm@gmail.com>
2020-10-07 18:11:35 +0900

    fontawesome 최적화

---
+++ src/assets/icons/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import 'vue-awesome/icons/cog'
+import 'vue-awesome/icons/angle-left'
+import 'vue-awesome/icons/angle-right'
+import 'vue-awesome/icons/trash-alt'
+import 'vue-awesome/icons/folder-open'
+import 'vue-awesome/icons/link'
+import 'vue-awesome/icons/forward'
+import 'vue-awesome/icons/minus'
+import 'vue-awesome/icons/plus'
+import 'vue-awesome/icons/bolt'
+import 'vue-awesome/icons/plug'
+import 'vue-awesome/icons/eye-slash'
+import 'vue-awesome/icons/fire-extinguisher'
+import 'vue-awesome/icons/bug'
+import 'vue-awesome/icons/info-circle'
+import 'vue-awesome/icons/archive'
+import 'vue-awesome/icons/save'
+import 'vue-awesome/icons/file-import'
+import 'vue-awesome/icons/times'
+import 'vue-awesome/icons/edit'
+import 'vue-awesome/icons/grip-vertical'
+import 'vue-awesome/icons/trash'
+import 'vue-awesome/icons/bars'
+import 'vue-awesome/icons/file-export'
+import Icon from 'vue-awesome/components/Icon'
+
+// eslint-disable-next-line vue/component-definition-name-casing
+Vue.component('v-icon', Icon)
+
+export default Icon
